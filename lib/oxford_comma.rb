a29d33302db7dc92989c5ad650ac24210fb9adb7
jaf7 <janthonyfields@gmail.com>
2018-01-17 20:04:19 -0500

    Used conditionals to create Oxford comma strings

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
 
+  if array.size > 1
+    array << "and ".concat(array[-1])
+    array.delete_at(-2)
+
+    if array.size > 2
+      array.join(", ")
+    else
+      array.join(" ")
+    end
+
+  else
+    array.join
+  end
+  
 end
